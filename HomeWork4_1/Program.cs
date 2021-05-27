513ddfb0751dd1a01c56c88a88421e3a3e168322
Александр <sasha21l@mail.ru>
2021-05-27 07:12:01 +0500

    HomeWork4_1

---
+++ HomeWork4_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork4_1
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            int startId = 4;
+            int endId = 13;
+            var tasks = new List<Task<Post>>();
+
+            // Создаем таски
+            for (int id = startId; id <= endId; id++)
+            {
+                tasks.Add(HttpHandler.GetPostAsync(id));
+            }
+            try
+            {
+                var postsList = (await Task.WhenAll(tasks.Where(t => t.IsFaulted == false))).ToList();
+                await FileHandler.WriteToFile(postsList);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
